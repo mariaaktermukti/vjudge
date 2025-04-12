385d5e0ad8f2c71e8c52d9dab6448ae7649b11ae
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-04-12 22:12:04 +0600

    Add files via upload

---
+++ A_Heat_Wave.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int X, Y;
+    scanf("%d %d", &X, &Y);
+    if (Y > X) {
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+    return 0;
+}
