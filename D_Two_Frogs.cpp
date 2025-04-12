56167a8811c5fd7d27f163718841d124df74988e
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-04-12 22:20:35 +0600

    Add files via upload

---
+++ D_Two_Frogs.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        int n, a, b;
+        cin >> n >> a >> b;
+        if(abs(a - b) % 2 == 0) {
+            cout << "YES" << "\n";
+        } else {
+            cout << "NO" << "\n";
+        }
+    }
+    return 0;
+}
