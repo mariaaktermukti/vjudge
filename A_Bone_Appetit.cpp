182f75feab3b345bb30853d15cabf2ca2fa5bfba
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-04-22 21:41:07 +0600

    Add files via upload

---
+++ A_Bone_Appetit.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+    int X, Y;
+    cin >> X >> Y;
+    int total = N * X + M * Y;
+    cout << total << "\n";
+    return 0;
+}
