4217a90250db88f7f4f0f52b96793b3dd8283921
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-06 20:31:52 +0600

    Add files via upload

---
+++ Minimum_Wage.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int X;
+    cin >> X;
+    if (X > 11) 
+    {
+        cout << "YES\n";
+    } 
+    else 
+    {
+        cout << "NO\n";
+    }
+    return 0;
+}
