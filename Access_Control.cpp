b797ae6138a1a0f22bfc3d5adfc3fafc528ebc04
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-05-27 21:40:02 +0600

    Add files via upload

--- Access_Control.cpp
+++ Access_Control.cpp
@@ -4,35 +4,41 @@ using namespace std;
 
 int main() 
 {
-    int T; 
+    int T;
     cin >> T;
+
     while (T--) 
     {
         int N, X;
+        cin >> N >> X;
         string S;
-        cin >> N >> X >> S;
+        cin >> S;
 
-        int swipes = 0; 
+        int swipesLeft = 0;
         bool valid = true;
 
         for (char c : S) 
         {
             if (c == '1') 
             {
-                swipes = X; 
+                swipesLeft = X;
             } 
             else 
             { 
-                if (swipes == 0) 
+                if (swipesLeft > 0) 
+                {
+                    swipesLeft--; 
+                } 
+                else 
                 {
                     valid = false; 
                     break;
                 }
-                swipes--;
             }
         }
 
         cout << (valid ? "YES" : "NO") << '\n';
     }
+
     return 0;
 }
